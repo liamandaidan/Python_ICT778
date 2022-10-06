1a3f2927ce4a34d8fcd5584096589e3fdbabe743
Liam <liammclaughlinis@gmail.com>
2022-10-06 12:36:50 -0600

    A2 q2c

--- A2/Assignment2.py
+++ A2/Assignment2.py
@@ -1,7 +1,80 @@
-def isPrime(number) -> bool:
-    """"""
+
+def isPrime(number: int) -> bool:
+    """Q1. Will check number to make sure it is prime.
+
+    Args:
+        number (int): Checked prime
+
+    Returns:
+        bool: T/F depening on if the provided num is prime.
+    """
+    if not isinstance(number, int):
+        raise TypeError
+
     output = (number % 1 == 0 and number % number == 0)
     for x in range(2, number):
         if number % x == 0:
             return False
+    print(output)
     return output
+
+
+def listComprehensionOdd(x=9, y=511, z=2) -> list[int]:
+    """Q2.a List of odd integers between 8-512
+
+    Args:
+        x (int, optional): Index to start at. Defaults to 9.
+        y (int, optional): Index to end before. Defaults to 511.
+        z (int, optional): Offset index. Defaults to 2.
+
+    Returns:
+        list[int]: List of odd numbers.
+    """
+    return [i for i in range(x, y, z)]
+
+
+def listComprehensionPrime(x=10, y=511, z=2) -> list[int]:
+    """Q2.b List of all comprehensions checking for prime nums
+
+    Args:
+        x (int, optional): Index to start at. Defaults to 9.
+        y (int, optional): Index to end before. Defaults to 511.
+        z (int, optional): Offset index. Defaults to 2.
+
+    Returns:
+        list[int]: List of prime numbers.
+    """
+    return [i for i in range(x, y, z) if isPrime(i)]
+
+
+def listComprehensionSum1(x=10, y=511, z=2) -> list[int]:
+    """Q3.c.1 Pretty complicated comprehension where i turn the values into tuples. Iterate through each tuple, look for the 0th one and add sum of tuples previously.
+
+    Args:
+        x (int, optional): Index to start at. Defaults to 10.
+        y (int, optional): Index to end before. Defaults to 511.
+        z (int, optional): Offset index. Defaults to 2.
+
+    Returns:
+        list[int]: A list of the sum of numbers.
+    """
+    output = []
+    sum = -1
+    test = [p for o in (range(x, y, z)) for p in enumerate(str(o))]
+    #n*('key', 'val')
+    for key, val in test:
+        # each time we loop back our tuple will start at 0. Take sum
+        if(sum == -1):
+            # first run
+            sum = int(val)
+        elif(key == 0):
+            output.append(sum)
+            # reset sum for next iteration
+            sum = int(val)
+        else:
+            sum += int(val)
+    # add final index for last val
+    output.append(sum)
+    return output
+
+
--- A2/driver.py
+++ A2/driver.py
@@ -1,11 +1,39 @@
 import Assignment2 as a2
+def prime():
+    primeNums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 97]
+    flag = True
+    for x in range(len(primeNums)):
+        if not a2.isPrime(primeNums[x]):
+            print('not prime num: {}'.format(primeNums[x]))
+            flag = False
+            break
+    if flag:
+        print('All nums provided are prime!')
+        
+def listSum(x=10, y=511, z=2) -> list[int]:
+    """generate sum from for loops. Used to check the list comprehension above.
 
-primeNums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 97]
-flag = True
-for x in range(len(primeNums)):
-    if not a2.isPrime(primeNums[x]):
-        print('not prime num: {}'.format(primeNums[x]))
-        flag = False
-        break
-if flag:
-    print('All nums provided are prime!')
+    Args:
+        x (int, optional): Index to start at. Defaults to 10.
+        y (int, optional): Index to end before. Defaults to 511.
+        z (int, optional): Offset index. Defaults to 2.
+
+    Returns:
+        list[int]: output of all sums
+    """
+    output = []
+    sum = 0
+    for i in (range(x, y, z)):
+        for p in str(i):
+            sum += int(p)
+        output.append(sum)
+        sum = 0
+    return output
+        
+#print(a2.listComprehensionOdd())
+#print(a2.listComprehensionPrime())
+f1 = a2.listComprehensionSum1()
+print('==================')
+f2 = listSum()
+print(f1==f2)
+
