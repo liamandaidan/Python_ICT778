e5178a90fd0d84cf11fd99cec9fa3f6c7c9362c7
Liam <liammclaughlinis@gmail.com>
2022-10-16 19:59:47 -0600

    day2prog

--- A2/Assignment2.py
+++ A2/Assignment2.py
@@ -47,9 +47,11 @@ def listComprehensionPrime(x=10, y=511, z=2) -> list[int]:
     return [i for i in range(x, y, z) if isPrime(i)]
 
 
-def listComprehensionSum1(x=10, y=511, z=2) -> list[int]:
-    """Q3.c.1 Pretty complicated comprehension where i turn the values into tuples. Iterate through each tuple, look for the 0th one and add sum of tuples previously.
-
+def listComprehensionSum1(x=9, y=512) -> list[int]:
+    """Q3.c. Pretty complicated comprehension where i turn the values into tuples. Iterate through each tuple, 
+        look for the 0th one and add sum of tuples previously.
+        It could probably be done better, but I couldnt find another solution at the moment.
+        Please check function ListSum as that is the way it should be done lol.
     Args:
         x (int, optional): Index to start at. Defaults to 10.
         y (int, optional): Index to end before. Defaults to 511.
@@ -59,15 +61,15 @@ def listComprehensionSum1(x=10, y=511, z=2) -> list[int]:
         list[int]: A list of the sum of numbers.
     """
     output = []
-    sum = -1
-    test = [p for o in (range(x, y, z)) for p in enumerate(str(o))]
-    #n*('key', 'val')
+    sum = -1 #set first value to -1 to trigger first if in for loop.
+    test = [p for o in (range(x, y)) for p in enumerate(str(o))]
+    #Example of our values. n*('key', 'val')
     for key, val in test:
         # each time we loop back our tuple will start at 0. Take sum
         if(sum == -1):
             # first run
             sum = int(val)
-        elif(key == 0):
+        elif(key == 0): #if key is 0, that means it is a new number. Therfore take previous values.
             output.append(sum)
             # reset sum for next iteration
             sum = int(val)
@@ -75,6 +77,41 @@ def listComprehensionSum1(x=10, y=511, z=2) -> list[int]:
             sum += int(val)
     # add final index for last val
     output.append(sum)
+
     return output
 
+def listSum(x=9, y=512) -> list[int]:
+    """Generate sum from for loops. Used to check the list comprehension above.
 
+    Args:
+        x (int, optional): Index to start at. Defaults to 10.
+        y (int, optional): Index to end before. Defaults to 511.
+        z (int, optional): Offset index. Defaults to 2.
+
+    Returns:
+        list[int]: output of all sums
+    """
+    output = []
+    for i in (range(x, y)):#get first integer eg -> 12
+        s = [] #Store one iteration of integer values eg -> [1,2]
+        for p in str(i):
+            s.append(int(p)) 
+        output.append(sum(s)) #add our values to output eg -> 3
+    return output
+
+def divisibleByThree(x=9, y=512, z=3) -> list[int]:
+    """Q2.d Check a list of numbers and deterimine if it is divisable by z.
+
+    Args:
+        x (int, optional): Start value. Defaults to 9.
+        y (int, optional): End value. Defaults to 512.
+        z (int, optional): Is divisible by. Defaults to 3.
+
+    Returns:
+        list[int]: A list of ints divisible by z.
+    """
+    return [i for i in range(x, y) if i % z == 0]
+
+def readingBooks(titleList, authorList, yearList):
+    
+    pass
--- A2/driver.py
+++ A2/driver.py
@@ -1,4 +1,7 @@
 import Assignment2 as a2
+# q1
+
+
 def prime():
     primeNums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 97]
     flag = True
@@ -9,31 +12,24 @@ def prime():
             break
     if flag:
         print('All nums provided are prime!')
-        
-def listSum(x=10, y=511, z=2) -> list[int]:
-    """generate sum from for loops. Used to check the list comprehension above.
 
-    Args:
-        x (int, optional): Index to start at. Defaults to 10.
-        y (int, optional): Index to end before. Defaults to 511.
-        z (int, optional): Offset index. Defaults to 2.
 
-    Returns:
-        list[int]: output of all sums
-    """
-    output = []
-    sum = 0
-    for i in (range(x, y, z)):
-        for p in str(i):
-            sum += int(p)
-        output.append(sum)
-        sum = 0
-    return output
-        
-#print(a2.listComprehensionOdd())
-#print(a2.listComprehensionPrime())
+# print(a2.listComprehensionOdd())
+# print(a2.listComprehensionPrime())
 f1 = a2.listComprehensionSum1()
-print('==================')
-f2 = listSum()
-print(f1==f2)
+# print(a2.divisibleByThree())
+f2 = a2.listSum()
+x = f1 == f2
+print(x)
 
+title = []
+author = []
+year = []
+for index in range(1,11):
+    title.append('Book{}'.format(index))
+    author.append('Author{}'.format(index))
+    year.append(2000+index)
+
+print(title)
+print(author)
+print(year)
